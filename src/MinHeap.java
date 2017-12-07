a93de028b2de035fe06161cfdb1d98bb05ca9dc7
bobbyjonesPro <serranod7@students.rowan.edu>
2017-12-06 21:22:45 -0500

    Update MinHeap.java

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -32,19 +32,18 @@ public class MinHeap implements MyHeap {
 		
 		if(path.charAt(0) == '0'){
 			insert.setLeftChild(newChild);
+			
 		}else{
 			insert.setRightChild(newChild);
 		}
 		
-		while(newChild != root){
+		newChild.setParent(insert);
+		
+		while(newChild.getParent() != null){
 			Node parent = newChild.getParent();
+			System.out.println(parent);
 			if(parent.getData().compareTo(newChild.getData()) > 0){
-				//parent greater than new child
-				Comparable swapSpace;
-				swapSpace = parent.getData();
-				parent.setData(newChild.getData());
-				newChild.setData(swapSpace);
-				
+				swap(parent, newChild);
 			}
 			else{
 				break;
@@ -57,7 +56,7 @@ public class MinHeap implements MyHeap {
 		
 	}
 	
-	@SuppressWarnings({ "rawtypes", "unchecked", "unused" })
+	@SuppressWarnings({ "unchecked" })
 	public boolean deleteMin(){
 		Node lastNode = root;
 		
@@ -72,36 +71,46 @@ public class MinHeap implements MyHeap {
 			}
 			path = path.substring(1);	
 		}
-		
-		if(path.charAt(0) == '0'){
-			root.setData(lastNode.getLeftChild().getData());
-			lastNode.setLeftChild(null);
-		}else{
-			root.setData(lastNode.getRightChild().getData());
-			lastNode.setRightChild(null);
-		}
-		numEntries--;
-		Node temp = root;
-		while(temp.getLeftChild() != null){
-			Node leftChild = temp.getLeftChild();
-			Node rightChild = temp.getRightChild();
-			
-			if(leftChild.getData().compareTo(rightChild.getData()) > 0){
-				//right child is smallest else left is
-				if(rightChild.getData().compareTo(temp.getData()) > 0){
-					swap(temp, rightChild);
-					temp = rightChild;
-				}else{
-					break;
-				}
+		if (path.length() == 1){
+			if(path.charAt(0) == '0'){
+				Node rootLeft = root.getLeftChild();
+				Node rootRight = root.getRightChild();
+				root = lastNode.getLeftChild();
+				root.setLeftChild(rootLeft);
+				root.setRightChild(rootRight);
+				lastNode.setLeftChild(null);
 			}else{
-				if(leftChild.getData().compareTo(temp.getData()) > 0){
-					swap(temp, leftChild);
-					temp = leftChild;
+				Node rootLeft = root.getLeftChild();
+				Node rootRight = root.getRightChild();
+				root = lastNode.getRightChild();
+				root.setLeftChild(rootLeft);
+				root.setRightChild(rootRight);
+				lastNode.setRightChild(null);
+			}
+			numEntries--;
+			Node temp = root;
+			while(temp.getLeftChild() != null){
+				
+				Node leftChild = temp.getLeftChild();
+				Node rightChild = temp.getRightChild();
+				
+				if(rightChild != null && leftChild.getData().compareTo(rightChild.getData()) > 0){
+					//right child is smallest else left is
+					if(rightChild.getData().compareTo(temp.getData()) < 0){
+						swap(temp, rightChild);
+					}else{
+						break;
+					}
 				}else{
-					break;
+					if(leftChild.getData().compareTo(temp.getData()) < 0){
+						swap(temp, leftChild);
+					}else{
+						break;
+					}
 				}
 			}
+		}else{
+			root = new Node();
 		}
 		
 		return true;
@@ -124,24 +133,23 @@ public class MinHeap implements MyHeap {
 				}
 				parent = key.getParent();
 			}
-		}else if(key.getLeftChild().getData().compareTo(key) > 0 || key.getLeftChild().getData().compareTo(key) > 0){
+		}else if(key.getLeftChild().getData().compareTo(key.getData()) > 0 || key.getLeftChild().getData().compareTo(key.getData()) > 0){
 			Node temp = key;
 			while(temp.getLeftChild() != null){
+				
 				Node leftChild = temp.getLeftChild();
 				Node rightChild = temp.getRightChild();
 				
-				if(leftChild.getData().compareTo(rightChild.getData()) > 0){
+				if(rightChild != null && leftChild.getData().compareTo(rightChild.getData()) > 0){
 					//right child is smallest else left is
-					if(rightChild.getData().compareTo(temp.getData()) > 0){
+					if(rightChild.getData().compareTo(temp.getData()) < 0){
 						swap(temp, rightChild);
-						temp = rightChild;
 					}else{
 						break;
 					}
 				}else{
-					if(leftChild.getData().compareTo(temp.getData()) > 0){
+					if(leftChild.getData().compareTo(temp.getData()) < 0){
 						swap(temp, leftChild);
-						temp = leftChild;
 					}else{
 						break;
 					}
@@ -157,7 +165,6 @@ public class MinHeap implements MyHeap {
 		
 		String path = Integer.toBinaryString(numEntries);
 		path = path.substring(1);
-		
 		while(path.length() > 1){
 			if(path.charAt(0) == '1'){
 				lastNode = lastNode.getRightChild();
@@ -166,36 +173,48 @@ public class MinHeap implements MyHeap {
 			}
 			path = path.substring(1);	
 		}
-		
-		if(path.charAt(0) == '0'){
-			del.setData(lastNode.getLeftChild().getData());
-			lastNode.setLeftChild(null);
-		}else{
-			del.setData(lastNode.getRightChild().getData());
-			lastNode.setRightChild(null);
-		}
-		numEntries--;
-		Node temp = del;
-		while(temp.getLeftChild() != null){
-			Node leftChild = temp.getLeftChild();
-			Node rightChild = temp.getRightChild();
-			
-			if(leftChild.getData().compareTo(rightChild.getData()) > 0){
-				//right child is smallest else left is
-				if(rightChild.getData().compareTo(temp.getData()) > 0){
-					swap(temp, rightChild);
-					temp = rightChild;
-				}else{
-					break;
-				}
+			if (path.length() == 1){
+			if(path.charAt(0) == '0'){
+				Node delLeft = del.getLeftChild();
+				Node delRight = del.getRightChild();
+				del = lastNode.getLeftChild();
+				del.setLeftChild(delLeft);
+				del.setRightChild(delRight);
+				lastNode.setLeftChild(null);
 			}else{
-				if(leftChild.getData().compareTo(temp.getData()) > 0){
-					swap(temp, leftChild);
-					temp = leftChild;
+				Node delLeft = del.getLeftChild();
+				Node delRight = del.getRightChild();
+				del = lastNode.getRightChild();
+				del.setLeftChild(delLeft);
+				del.setRightChild(delRight);
+				lastNode.setRightChild(null);
+			}
+			
+			numEntries--;
+			Node temp = del;
+			while(temp.getLeftChild() != null){
+				
+				Node leftChild = temp.getLeftChild();
+				Node rightChild = temp.getRightChild();
+				
+				if(rightChild != null && leftChild.getData().compareTo(rightChild.getData()) > 0){
+					//right child is smallest else left is
+					if(rightChild.getData().compareTo(temp.getData()) < 0){
+						swap(temp, rightChild);
+					}else{
+						break;
+					}
 				}else{
-					break;
+					if(leftChild.getData().compareTo(temp.getData()) < 0){
+						swap(temp, leftChild);
+					}else{
+						break;
+					}
 				}
 			}
+		}else{
+			root = new Node();
+			System.out.print("shit");
 		}
 		return true;
 	}
@@ -214,14 +233,134 @@ public class MinHeap implements MyHeap {
 		return root.getData();
 	}
 	
-	@SuppressWarnings("rawtypes")
-	private void swap(Node leftHand, Node rightHand){
-		Comparable swapSpace;
-		swapSpace = leftHand.getData();
-		leftHand.setData(rightHand.getData());
-		rightHand.setData(swapSpace);
-	}
-	
+	private void swap(Node b, Node c){
+		if(b != root){
+			Node a = b.getParent();
+			
+			if (b.getLeftChild() == c){
+				Node d = b.getRightChild();
+				Node e = c.getLeftChild();
+				Node f = c.getRightChild();
+				
+				if(a.getLeftChild() == b){
+					a.setLeftChild(c);
+					c.setParent(a);
+				}else{
+					a.setRightChild(c);
+					c.setParent(a);
+				}
+				
+				b.setLeftChild(e);
+				b.setRightChild(f);
+				
+				if(e != null){
+					e.setParent(b);
+				}
+				
+				if(f != null){
+					f.setParent(b);
+				}
+				
+				b.setParent(c);
+				
+				c.setLeftChild(b);
+				c.setRightChild(d);
+				
+				if(d != null){
+					d.setParent(c);
+				}
+			}else{
+				Node d = b.getLeftChild();
+				Node e = c.getLeftChild();
+				Node f = c.getRightChild();
+				
+				if(a.getLeftChild() == b){
+					a.setLeftChild(c);
+					c.setParent(a);
+				}else{
+					a.setRightChild(c);
+					c.setParent(a);
+				}
+				
+				b.setLeftChild(e);
+				b.setRightChild(f);
+				
+				if(e != null){
+					e.setParent(b);
+				}
+				
+				if(f != null){
+					f.setParent(b);
+				}
+				
+				b.setParent(c);
+				
+				c.setRightChild(d);
+				c.setLeftChild(b);
+				System.out.print(d==c);
+				if(d != null){
+					d.setParent(c);
+				}
+			}
+		}else{
+			if (b.getLeftChild() == c){
+				Node d = b.getRightChild();
+				Node e = c.getLeftChild();
+				Node f = c.getRightChild();
+				
+				c.setParent(null);
+				root = c;
+				
+				b.setLeftChild(e);
+				b.setRightChild(f);
+				
+				if(e != null){
+					e.setParent(b);
+				}
+				
+				if(f != null){
+					f.setParent(b);
+				}
+				
+				b.setParent(c);
+				
+				c.setLeftChild(b);
+				c.setRightChild(d);
+				
+				if(d != null){
+					d.setParent(c);
+				}
+			}else{
+				Node d = b.getLeftChild();
+				Node e = c.getLeftChild();
+				Node f = c.getRightChild();
+				
+				c.setParent(null);
+				root = c;
+				
+				b.setLeftChild(e);
+				b.setRightChild(f);
+				
+				if(e != null){
+					e.setParent(b);
+				}
+				
+				if(f != null){
+					f.setParent(b);
+				}
+				
+				b.setParent(c);
+				
+				c.setRightChild(b);
+				c.setLeftChild(d);
+				
+				if(d != null){
+					d.setParent(c);
+				}
+			}
+		}
+	}	
+}
 
 	
 }
